3c760dfccf9f5b7a451c414997249d9d55ce2517
Gianluigi Forte <koalakoker@gmail.com>
2024-03-11 14:10:26 +0100

    Crating base data structure

--- MyGui.py
+++ MyGui.py
@@ -4,13 +4,29 @@ from tkinter import messagebox
 class MyGui():
   def __init__(self) -> None:
     self.root = tk.Tk()
-    self.root.geometry("800x600")
+    self.root.geometry("400x600")
 
-    self.label = tk.Label(self.root, text="Label", font=("Arial",16), justify ="left")
-    self.label.pack(padx=10, pady=5, anchor="w")
+    self.verticalLayout = tk.Frame(self.root)
+    self.verticalLayout.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-    self.quit = tk.Button(self.root, text="Quit", justify="left", command=self.closeWin)
-    self.quit.pack(padx=10, pady=5, anchor="sw")
+    self.row1 = tk.Frame(self.verticalLayout)
+    self.row1.pack(side=tk.TOP, fill=tk.X)
+
+    self.label = tk.Label(self.row1, text="PWM frequency", font=("Arial",16), justify ="left")
+    self.label.pack(padx=10, pady=5, side=tk.LEFT, anchor="n", expand=True)
+
+    self.inputValue = tk.IntVar(value=10000)
+    self.input = tk.Entry(self.row1, textvariable=self.inputValue)
+    self.input.pack(padx=10, pady=5, side=tk.LEFT, anchor="n", expand=True)
+
+    self.unit = tk.Label(self.row1, text="Hz", font=("Arial",16), justify ="left")
+    self.unit.pack(padx=10, pady=5, side=tk.LEFT, anchor="n",expand=True)
+
+    self.row2 = tk.Frame(self.verticalLayout)
+    self.row2.pack(side=tk.TOP, fill=tk.X)
+
+    self.quit = tk.Button(self.row2, text="Quit", justify="left", command=self.closeWin)
+    self.quit.pack(padx=10, pady=5, side=tk.LEFT, anchor="s", expand=True)
 
     self.root.bind('<Escape>', self.closeWin)
     self.root.mainloop()
